f97d2c4a9824397dc62e5feecdab4fd6025c1eef
DevilOper <DevilOper@localhost>
2021-05-20 22:08:22 +0300

    Update 20-05-2021 2208

--- local/templates/dreamcity-plastilin/footer.php
+++ local/templates/dreamcity-plastilin/footer.php
@@ -4,8 +4,8 @@ IncludeTemplateLangFile(__FILE__);
 </div>
 
     <?$APPLICATION->AddHeadScript("https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js");?>
-    <?$APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH . "assets/js/libs.min.js");?>
-    <?$APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH . "assets/js/old.js");?>
-    <?$APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH . "assets/js/main.js");?>
+    <?$APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH . DIRECTORY_SEPARATOR . "assets/js/libs.min.js");?>
+    <?$APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH . DIRECTORY_SEPARATOR . "assets/js/old.js");?>
+    <?$APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH . DIRECTORY_SEPARATOR . "assets/js/main.js");?>
 </body>
 </html>
