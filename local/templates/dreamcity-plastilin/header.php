b1f82928fa9803757a1355a37ce763fb7b13b636
DevilOper <DevilOper@localhost>
2021-05-25 22:13:07 +0300

    Update header component on tamplte

--- local/templates/dreamcity-plastilin/header.php
+++ local/templates/dreamcity-plastilin/header.php
@@ -22,20 +22,20 @@ $c404 = $APPLICATION->getPageProperty('class_404');
                     <div class="menuPane" id="burger-pane">
                         <div class="menuPane__inner">
                             <div class="menuPane__logo"><a class="menuLogo" href="#"><span class="menuLogo__desktop"><img
-                                                src="/local/img/41441bb52226d181cdee3f4fb2ddf21c.svg" alt="Лого"></span><span
-                                            class="menuLogo__moibile"><img src="/local/img/4b4f6571f166bb19037e6d41350876ff.svg"
+                                                src="/local/local/img/41441bb52226d181cdee3f4fb2ddf21c.svg" alt="Лого"></span><span
+                                            class="menuLogo__moibile"><img src="/local/local/img/4b4f6571f166bb19037e6d41350876ff.svg"
                                                                            alt="Лого"></span></a></div>
                             <div class="menuPane__btn">
                                 <button class="arrBtn" id="burger-btn"><span class="arrBtn__inner"><span class="arrBtn__text">Меню
                       </span><span class="arrBtn__ico">
                         <svg class="arrBtn__svgr" width="50px" height="50px">
-                          <use xlink:href="/local/img/svg/icons.svg#menuBurger"></use>
+                          <use xlink:href="/local/local/img/svg/icons.svg#menuBurger"></use>
                         </svg><span class="arrBtn__abs">
                           <svg width="32px" height="17px">
-                            <use xlink:href="/local/img/svg/icons.svg#arrowBurger"></use>
+                            <use xlink:href="/local/local/img/svg/icons.svg#arrowBurger"></use>
                           </svg></span><span class="arrBtn__cls">
                           <svg width="16px" height="16px">
-                            <use xlink:href="/local/img/svg/icons.svg#cls"></use>
+                            <use xlink:href="/local/local/img/svg/icons.svg#cls"></use>
                           </svg></span></span></span></button>
                             </div>
                         </div>
@@ -59,13 +59,13 @@ $c404 = $APPLICATION->getPageProperty('class_404');
                             <button class="aroundIco" data-modal-open="m-4">
                                 <div class="aroundIco__abs">
                                     <svg>
-                                        <use xlink:href="/local/img/svg/icons.svg#ligntC"></use>
+                                        <use xlink:href="/local/local/img/svg/icons.svg#ligntC"></use>
                                     </svg>
                                 </div>
                                 <div class="aroundIco__inner">
                                     <div class="aroundIco__center">
                                         <svg width="19px" height="18px">
-                                            <use xlink:href="/local/img/svg/icons.svg#search"></use>
+                                            <use xlink:href="/local/local/img/svg/icons.svg#search"></use>
                                         </svg>
                                     </div>
                                 </div>
@@ -77,13 +77,13 @@ $c404 = $APPLICATION->getPageProperty('class_404');
                             <button class="aroundIco" data-modal-open="m-1">
                                 <div class="aroundIco__abs">
                                     <svg>
-                                        <use xlink:href="/local/img/svg/icons.svg#ligntC"></use>
+                                        <use xlink:href="/local/local/img/svg/icons.svg#ligntC"></use>
                                     </svg>
                                 </div>
                                 <div class="aroundIco__inner">
                                     <div class="aroundIco__center">
                                         <svg width="18px" height="18px">
-                                            <use xlink:href="/local/img/svg/icons.svg#phone"></use>
+                                            <use xlink:href="/local/local/img/svg/icons.svg#phone"></use>
                                         </svg>
                                     </div>
                                 </div>
@@ -131,12 +131,12 @@ $c404 = $APPLICATION->getPageProperty('class_404');
                                                            placeholder="Адрес, метро, район или id"><span class="customInput__cls">
                               <button class="customInput__clear" data-dd-clear>
                                 <svg width="20px" height="20px">
-                                  <use xlink:href="/local/img/svg/icons.svg#cls"></use>
+                                  <use xlink:href="/local/local/img/svg/icons.svg#cls"></use>
                                 </svg>
                               </button></span><span class="customInput__btn">
                               <button class="fieldBox__btn"><span class="fieldBox__bi">
                                   <svg width="23px" height="22px">
-                                    <use xlink:href="/local/img/svg/icons.svg#search"></use>
+                                    <use xlink:href="/local/local/img/svg/icons.svg#search"></use>
                                   </svg></span></button></span>
                                                     <div class="fieldBox__dd" data-dd>
                                                         <div class="ddCollapsed">
@@ -202,3 +202,72 @@ $c404 = $APPLICATION->getPageProperty('class_404');
             </div>
         </div>
     </header>
+    <main class="siteContent">
+        <section class="siteSection siteSection-main">
+            <div class="screenBox">
+                <div class="dream-container">
+                    <div class="mainBanner">
+                        <div class="mainBanner__inner">
+                            <div class="breadcrumbs breadcrumbs-adapt" style="max-width: 1319px;">
+                                <a href="#">Главная</a>
+                                <span>Коммерческая недвижимость</span>
+                            </div>
+                            <div class="mainBanner__info">
+                                <div class="mainBanner__textfield mainBanner__textfield-static">
+                                    <div class="sup-title">Dream <span class="hl">City</span></div>
+                                    <h1 class="title-h1 title-h1--mt"><span class="hl">Баннер</span> о коммерческой недвижимости</h1>
+                                    <p class="simple-text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+                                        ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes,
+                                        nascetur ridiculus musLorem ipsum dolor sit amet, consectetuer adipiscing elit</p>
+                                </div>
+                                <div class="mainBanner__bot">
+                                    <div class="searchBox">
+                                        <div class="fieldBox" data-input-field="">
+                                            <input class="customInput" type="search" id="s-1" data-custom-input="" data-input-dd="" placeholder="Адрес, метро, район или id"><span class="customInput__cls">
+                          <button class="customInput__clear" data-dd-clear="">
+                            <svg width="20px" height="20px">
+                              <use xlink:href="/local/img/svg/icons.svg#cls"></use>
+                            </svg>
+                          </button></span><span class="customInput__btn">
+                          <button class="fieldBox__btn"><span class="fieldBox__bi">
+                              <svg width="23px" height="22px">
+                                <use xlink:href="/local/img/svg/icons.svg#search"></use>
+                              </svg></span></button></span>
+                                            <div class="fieldBox__dd" data-dd="">
+                                                <div class="ddCollapsed">
+                                                    <div class="ddCollapsedList"><a class="ddCollapsedList__item" href="#"><span class="hl">Моск</span>ва</a><a class="ddCollapsedList__item" href="#"><span class="hl">Моск</span>овский, поселок <span class="hl">Моск</span>овский, г. <span class="hl">Моск</span>ва</a><a class="ddCollapsedList__item" href="#"><span class="hl">Моск</span>али, Томский р-а, Иркутская обл.</a><a class="ddCollapsedList__item" href="#"><span class="hl">Моск</span>овский, г.
+                                                            Екатеринбург, Сверловская обл.</a></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="mainBanner__slider">
+                                <div class="radialSlider radialSlider-single">
+                                    <div class="radialSlider-inner">
+                                        <div class="radialSlider-inner--item __active-slide"><img src="./local/img/banner-1.3e9787592d4ef739f7e2a9e4b192a9eb.png" alt=""></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="mainBanner__map">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="botBar botBar-rel">
+                    <div class="dream-container">
+                        <button class="arrBtn arrBtn-white" data-modal-open="m-3"><span class="arrBtn__inner"><span class="arrBtn__text">Подбор недвижимости</span><span class="arrBtn__ico">
+                    <svg width="50px" height="50px">
+                      <use xlink:href="/local/img/svg/icons.svg#menuBurger"></use>
+                    </svg><span class="arrBtn__abs">
+                      <svg width="32px" height="17px">
+                        <use xlink:href="/local/img/svg/icons.svg#arrowBurger"></use>
+                      </svg></span><span class="arrBtn__cls">
+                      <svg width="16px" height="16px">
+                        <use xlink:href="/local/img/svg/icons.svg#cls"></use>
+                      </svg></span></span></span></button>
+                    </div>
+                </div>
+            </div>
+        </section>
