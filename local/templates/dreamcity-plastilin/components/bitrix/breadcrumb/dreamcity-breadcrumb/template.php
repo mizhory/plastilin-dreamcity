8188c2ebe2e8c3c85ad41bbc60f33be1c2efcda6
DevilOper <DevilOper@localhost>
2021-05-26 22:13:09 +0300

    Updated menu multilevel n breadcrumb

--- local/templates/dreamcity-plastilin/components/bitrix/breadcrumb/dreamcity-breadcrumb/template.php
+++ local/templates/dreamcity-plastilin/components/bitrix/breadcrumb/dreamcity-breadcrumb/template.php
@@ -5,7 +5,7 @@ if(count($arResult)<=1) return;
 <?$e = count($arResult)-1;?>
 <div class="breadcrumbs breadcrumbs-adapt" style="max-width: 1319px;">
     <?foreach($arResult as $k=>$arItem):?>
-    <?if($e<$k):?>
+    <?if($e>$k):?>
         <a href="<?=$arItem["LINK"]?>"><?=$arItem["TITLE"]?></a>
     <?else:?>
         <span><?=$arItem["TITLE"]?></span>
